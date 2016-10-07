e0e6d05264cbae6cbb4a3aef820393a87c361146
anwaukoni <anwaukoni@gmail.com>
2016-10-07 12:02:57 +0100

    refactored code some more

--- challenges/OpenSignalAlgo.js
+++ challenges/OpenSignalAlgo.js
@@ -5,22 +5,19 @@
 
   A[0]+A[1]+A[2] === A[4]+A[5]+A[6]
 
+  array = [500, -1, 1]
+  upperSum = 500
+  lowerSum = 0
+
+  equilibriumIdx = 0
+  upperSum = upperSum - 500
+  upperSum = 0
+  upperSum === lowerSum
+  done, return 0
+
   //Assume sum of zero elements === 0
 */
 
-
-
-
-
-// array = [500, -1, 1]
-// upperSum = 500
-// lowerSum = 0
-//
-// equilibriumIdx = 0
-// upperSum = upperSum - 500
-// upperSum = 0
-// upperSum === lowerSum
-// done, return 0
 
 const searchEquilibriumIndex = array => {
   if (!array || array.length === 0) return undefined;
@@ -28,7 +25,7 @@ const searchEquilibriumIndex = array => {
   let lowerSum = 0;
   let upperSum = array.reduce((a,b) => a + b );
 
-  for (var equilibriumIdx = 0; equilibriumIdx < array.length; equilibriumIdx++){
+  for (let equilibriumIdx = 0; equilibriumIdx < array.length; equilibriumIdx++){
 
     upperSum -= array[equilibriumIdx];
 
@@ -43,6 +40,6 @@ const searchEquilibriumIndex = array => {
 }
 
 
-console.log(searchEquilibriumIndex([-7,1,5,2,-4,3,0]));
-
+// console.log(searchEquilibriumIndex([-7,1,5,2,-4,3,0]));
+//
 // console.log(searchEquilibriumIndex([-28,1,2,3,4,5,6,7,500]));
